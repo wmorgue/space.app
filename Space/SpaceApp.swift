a6b874b05aeae67fef4f479bc38bf932ea6e962c
Nikita Rossik <born2lose@protonmail.com>
2021-12-02 10:39:07 +0300

    Initial Commit

---
+++ Space/SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceApp.swift
+//  Space
+//
+//  Created by Nikita Rossik on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
