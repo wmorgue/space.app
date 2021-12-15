3fe213ead997cc324d83cc34e43cd5bcb5da6f22
Nikita Rossik <born2lose@protonmail.com>
2021-12-15 22:47:50 +0300

    improvement bottom bar

--- Space/Model/TabItem.swift
+++ Space/Model/TabItem.swift
@@ -5,7 +5,7 @@
 //  Created by Nikita Rossik on 12/10/21.
 //
 
-import Foundation
+import SwiftUI
 
 
 struct TabItem: Identifiable {
@@ -13,15 +13,16 @@ struct TabItem: Identifiable {
 	let title: String
 	let iconName: String
 	let tab: CurrentTab
+	var color: Color
 }
 
 
 extension TabItem {
 	static var data = [
-		Self(title: "Learn Now", iconName: "house", tab: .home),
-		Self(title: "Explore", iconName: "magnifyingglass", tab: .explore),
-		Self(title: "Notifications", iconName: "bell", tab: .notifications),
-		Self(title: "Library", iconName: "rectangle.stack", tab: .library)
+		Self(title: "Learn Now", iconName: "house", tab: .home, color: .teal),
+		Self(title: "Explore", iconName: "magnifyingglass", tab: .explore, color: .blue),
+		Self(title: "Notifications", iconName: "bell", tab: .notifications, color: .red),
+		Self(title: "Library", iconName: "rectangle.stack", tab: .library, color: .pink)
 	]
 }
 
@@ -31,4 +32,9 @@ enum CurrentTab: String {
 	case explore
 	case notifications
 	case library
+}
+
+struct TabPreferenceKey: PreferenceKey {
+	static var defaultValue: CGFloat = .zero
+	static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) { value = nextValue() }
 }
--- Space/Views/TabBar.swift
+++ Space/Views/TabBar.swift
@@ -8,45 +8,32 @@
 import SwiftUI
 
 struct TabBar: View {
+	@State private var colorButtonBar: Color = .teal
 	@State private var selectedTab: CurrentTab = .home
+	@State private var tabItemWidth: CGFloat = .zero
 	
 	var body: some View {
 		ZStack {
-			
-			viewBody(selectedTab)
-			
-			HStack {
-				ForEach(TabItem.data) { item in
-					Button {
-						selectedTab = item.tab
-					} label: {
-						Label {
-							Text(item.title)
-								.font(.caption2)
-								.lineLimit(1)
-						} icon: {
-							Image(systemName: item.iconName)
-								.symbolVariant(.fill)
-								.font(.body.bold())
-								.frame(width: 80, height: 29)
-						}
-					}
-					.foregroundStyle(.secondary)
-				}
-				.frame(maxWidth: .infinity)
-			}
-			.padding(.top, 14)
-			.labelStyle(.vertically)
-			.frame(height: 88, alignment: .top)
-			.materialBackground(cornerRadius: 34)
-			.frame(maxHeight: .infinity, alignment: .bottom)
-			.ignoresSafeArea()
+			tabContent(selectedTab)
+			buttonsBar
+				.padding(.top, 14)
+				.labelStyle(.vertically)
+				.frame(height: 88, alignment: .top)
+				.materialBackground(cornerRadius: 34)
+				.background(
+					barOverlay(colorButtonBar, width: tabItemWidth)
+				)
+				.overlay(
+					barOverlay(colorButtonBar, width: tabItemWidth, isOverlay: true)
+				)
+				.frame(maxHeight: .infinity, alignment: .bottom)
+				.ignoresSafeArea()
 		}
 	}
 }
 
 extension TabBar {
-	func viewBody(_ tab: CurrentTab) -> some View {
+	func tabContent(_ tab: CurrentTab) -> some View {
 		Group {
 			switch tab {
 				case .home:
@@ -62,6 +49,76 @@ extension TabBar {
 					Text("Library")
 			}
 		}
+		.frame(maxWidth: .infinity, maxHeight: .infinity)
+	}
+	
+	private var buttonsBar: some View {
+		HStack {
+			ForEach(TabItem.data) { item in
+				Button {
+					withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+						selectedTab = item.tab
+						colorButtonBar = item.color
+					}
+				} label: {
+					Label {
+						Text(item.title)
+							.font(.caption2)
+							.lineLimit(1)
+					} icon: {
+						Image(systemName: item.iconName)
+							.symbolVariant(.fill)
+							.font(.body.bold())
+							.frame(width: 80, height: 29)
+					}
+				}
+				.foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
+				.blendMode(selectedTab == item.tab ? .overlay : .normal)
+				.overlay(
+					GeometryReader { proxy in
+						Color
+							.clear
+							.preference(key: TabPreferenceKey.self, value: proxy.size.width)
+					}
+				)
+				.onPreferenceChange(TabPreferenceKey.self) { value in
+					tabItemWidth = value
+				}
+			}
+			.frame(maxWidth: .infinity)
+		}
+	}
+	
+	// So scarry, but it's working
+	private func barOverlay(_ circleColor: Color, width: CGFloat, isOverlay: Bool = false) -> some View {
+		HStack {
+			if selectedTab == .library { Spacer() }
+			if selectedTab == .explore { Spacer() }
+			if selectedTab == .notifications {
+				Spacer()
+				Spacer()
+			}
+			switch isOverlay {
+				case true:
+					Rectangle()
+						.fill(circleColor)
+						.frame(width: 26, height: 4)
+						.cornerRadius(3)
+						.frame(width: width)
+						.frame(maxHeight: .infinity, alignment: .top)
+				case false:
+					Circle()
+						.fill(circleColor)
+						.frame(width: width)
+			}
+			if selectedTab == .home { Spacer() }
+			if selectedTab == .explore {
+				Spacer()
+				Spacer()
+			}
+			if selectedTab == .notifications { Spacer() }
+		}
+		.padding(.horizontal, 8)
 	}
 }
 
@@ -69,6 +126,7 @@ extension TabBar {
 struct TabBar_Previews: PreviewProvider {
 	static var previews: some View {
 		TabBar()
-		//			.preferredColorScheme(.dark)
+			.preferredColorScheme(.dark)
+			.previewInterfaceOrientation(.landscapeLeft)
 	}
 }
